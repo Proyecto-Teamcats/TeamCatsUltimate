de1a5226260a1b06d2e49c6eff699c83fbfa03b0
Kazzuki723 <87679992+Kazzuki723@users.noreply.github.com>
2022-01-27 20:01:31 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,11 +8,6 @@ connstr='username/pwd@database'
 conn = cx_Oracle.connect(connstr)
 curs = conn.cursor()
 
-curs.execute('select * from table1;')
-print curs.description
-for row in curs:
-   print row
-conn.close()
 
 def main():
     """Run administrative tasks."""
